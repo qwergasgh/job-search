a901da1706f324ac13676d90cc980caa6c567907
qwergasgh <qwergasgh@gmail.com>
2022-02-04 22:12:15 +0300

    fix imports

--- app/search.py
+++ app/search.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 from flask_login.utils import login_required, current_user
 from .forms import SearchForm, ParsingForm
-from .utils import Parsing
+from parsing import Parsing
 
 
 blueprint_search = Blueprint('blueprint_search', 
